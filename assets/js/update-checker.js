06a4355bcc4251bcb19bbb6fd1cca9414b07db97
BaoDDC <thaibao06061997@gmail.com>
2026-07-28 16:51:45 +0700

    Bump web version to v1.0.6 (web-only release)

--- assets/js/update-checker.js
+++ assets/js/update-checker.js
@@ -1,5 +1,5 @@
 (function () {
-  const CURRENT_VERSION = '1.0.5';
+  const CURRENT_VERSION = '1.0.6';
   window.APP_VERSION = CURRENT_VERSION;
 
   const SUPABASE_URL = 'https://ahcethtonjwktjtmxzog.supabase.co';
